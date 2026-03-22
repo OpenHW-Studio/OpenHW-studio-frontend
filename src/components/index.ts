bf89c0e0e990a1315dcf71f99a9711fe646d66a7
B Naga Krishna Manohar <krishnamanohar1179@gmail.com>
2026-03-22 19:21:21 +0530

    feat: add wokwi-analog-joystick component including interactive UI, simulation logic, manifest, and documentation.

--- src/components/index.ts
+++ src/components/index.ts
@@ -21,6 +21,7 @@ export { default as wokwiLcd2004I2C } from './wokwi-lcd2004-i2c';
 export { default as wokwiLdrModule } from './wokwi-ldr-module';
 export { default as max30102 } from './max30102';
 export { default as wokwiMembraneKeypad } from './wokwi-membrane-keypad';
+export { default as wokwiAnalogJoystick } from './wokwi-analog-joystick';
 
 
 
