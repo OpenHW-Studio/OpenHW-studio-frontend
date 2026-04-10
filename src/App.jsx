0c287a11b9c56db31a269de2a734059ef2af401a
MD Danish <110881758+danish9661@users.noreply.github.com>
2026-04-10 18:37:56 +0530

    Revert "Sagar/work"

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,6 @@ export default function App() {
                         <Route path="/gamification-simulator/:projectName" element={<GamificationSimulatorPage />} />
             {/* Guest accessible simulator */}
             <Route path="/simulator" element={<SimulatorPage />} />
-            <Route path="/simulator/share/:shareId" element={<SimulatorPage />} />
             <Route path="/:projectName/demo" element={<SimulatorPage />} />
             <Route path="/:projectName/guide" element={<ProjectGuidePage />} />
             <Route path="/:projectName/gamified-guide" element={<GamifiedProjectGuidePage />} />
