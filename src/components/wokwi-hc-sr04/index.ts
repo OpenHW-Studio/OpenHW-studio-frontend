f568c9a2f189249ea04781be3c2de695641f6432
B Naga Krishna Manohar <krishnamanohar1179@gmail.com>
2026-04-20 14:22:43 +0530

    feat: add HC-SR04 ultrasonic sensor component with logic and UI support

---
+++ src/components/wokwi-hc-sr04/index.ts
@@ -0,0 +1,9 @@
+import { HCSR04Logic } from './logic';
+import { HCSR04UI } from './ui';
+import manifest from './manifest.json';
+
+export default {
+    manifest,
+    Logic: HCSR04Logic,
+    UI: HCSR04UI
+};
