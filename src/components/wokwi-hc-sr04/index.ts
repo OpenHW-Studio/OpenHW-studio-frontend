67e060115389eb7dc0208f16eee264ae02cbbcfc
B Naga Krishna Manohar <krishnamanohar1179@gmail.com>
2026-04-21 10:58:06 +0530

    feat: add documentation and logic for HC-SR04, servo, and soil moisture sensor components

--- src/components/wokwi-hc-sr04/index.ts
+++ src/components/wokwi-hc-sr04/index.ts
@@ -1,5 +1,5 @@
 import { HCSR04Logic } from './logic';
-import { HCSR04UI, HCSR04ContextMenu } from './ui';
+import { HCSR04UI, HCSR04ContextMenu, BOUNDS } from './ui';
 import manifest from './manifest.json';
 import { doc } from './doc';
 
@@ -7,6 +7,7 @@ export default {
     manifest,
     Logic: HCSR04Logic,
     UI: HCSR04UI,
+    BOUNDS,
     ContextMenu: HCSR04ContextMenu,
     contextMenuDuringRun: true,
     doc
