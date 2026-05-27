00a8723e30d9c0c107230014a7f3bbff5e28b1cd
Satvik <satviksharma511@gmail.com>
2026-05-27 15:24:28 +0530

    PIR-motion-sensor

--- src/esp32/worker/execute.ts
+++ src/esp32/worker/execute.ts
@@ -16,7 +16,7 @@ import { PotentiometerLogic } from '@openhw/emulator/src/components/wokwi-potent
 import { HC595Logic as ShiftRegisterLogic } from '@openhw/emulator/src/components/openhw-74hc595/logic.ts';
 import { MembraneKeypadLogic } from '@openhw/emulator/src/components/wokwi-membrane-keypad/logic.ts';
 import { LCD1602Logic } from '@openhw/emulator/src/components/wokwi-LCD1602/logic.ts';
-import { PIRLogic } from '@openhw/emulator/src/components/wokwi-PIR-Motion-Sensor/logic.ts';
+import { PIRLogic } from '@openhw/emulator/src/components/PIR-Motion-Sensor/logic.ts';
 import { GasSensorLogic } from '@openhw/emulator/src/components/wokwi-gas-sensor/logic.ts';
 import { HCSR04Logic } from '@openhw/emulator/src/components/wokwi-hc-sr04/logic.ts';
 import { SoilMoistureSensorLogic } from '@openhw/emulator/src/components/wokwi-soil-moisture-sensor/logic.ts';
