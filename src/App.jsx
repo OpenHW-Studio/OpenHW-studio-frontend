db72e44f02e4aa65337c1dc1434f11574de39cf8
B Naga Krishna Manohar <krishnamanohar1179@gmail.com>
2026-08-18 18:28:54 +0530

    feat: implement contributors page and update project assets

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ import ComponentLab from "./pages/simulationpage/ComponentLab.jsx";
 const GradingPage = React.lazy(() => import("./pages/GradingPage.jsx"));
 import MaintenancePage from "./pages/MaintenancePage.jsx";
 import AboutUsNew from "./pages/AboutUsNewPage.jsx";
+import ContributorsPage from "./pages/ContributorsPage.jsx";
 import VisitorTracker from "./components/VisitorTracker.jsx";
 
 import { fetchMaintenanceStatus } from "./services/simulatorService.js";
@@ -192,6 +193,7 @@ export default function App() {
                 {/* Public Routes */}
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutUsNew />} />
+                <Route path="/contributors" element={<ContributorsPage />} />
                 <Route path="/examples" element={<ExamplesPage />} />
                 <Route path="/login" element={<UserLoginPage />} />
                 <Route
